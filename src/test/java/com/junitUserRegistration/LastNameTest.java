ce0c59203dc081a6e31195ec5152279ab554e160
Rachana <rachanarrachu28@gmail.com>
2020-10-27 20:16:49 +0530

    Refactor : code is refactored

---
+++ src/test/java/com/junitUserRegistration/LastNameTest.java
@@ -0,0 +1,40 @@
+package com.junitUserRegistration;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+public class LastNameTest {
+	static UserRegistration user;
+
+	@BeforeClass
+	public static void createUserRegistrationObj() {
+		user = new UserRegistration();
+		System.out.println("In before class: Last Name");
+	}
+
+	@AfterClass
+	public static void nullObj() {
+		System.out.println("In After class: Last Name");
+		user = null;
+	}
+
+	@Test
+	public void testLasttNameWithFirstLetterCapital_testAssertionHappy() {
+		boolean result = user.lastName("Rachu");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void testLastNameWithSpecialCharacter_testAssertionSad() {
+		boolean result = user.lastName("R@chu");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void testLastNameWithLessThanThreeCharacter_testAssertionSad() {
+		boolean result = user.lastName("Ra");
+		Assert.assertFalse(result);
+	}
+}
+
+
