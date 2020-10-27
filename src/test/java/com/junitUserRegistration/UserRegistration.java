7ab6a9eb9cb6474f17625d0135feb04525cb1588
Rachana <rachanarrachu28@gmail.com>
2020-10-27 22:07:04 +0530

    Added : Parameters are passed to test a Email

--- src/test/java/com/junitUserRegistration/UserRegistration.java
+++ src/test/java/com/junitUserRegistration/UserRegistration.java
@@ -7,5 +7,35 @@ public class UserRegistration {
 			boolean fname = Pattern.matches("^[A-Z]([a-z]{2,})$", firstName);
 			return fname;
 	}
+		public boolean lastName(String lastName) {
+			boolean lname = Pattern.matches("^[A-Z]([a-z]{2,})$", lastName);
+			return lname;
+	}
+		public  boolean email(String mail) {
+			boolean email = Pattern.matches("^[A-Za-z0-9]+([._%+-][0-9a-zA-Z]+)*@[A-Za-z0-9]+([.]([a-zA-Z]{2,3})*)+$", mail);
+			return email;
+		}
+		public  boolean mobileNum(String mNum) {
+			boolean mobileNum = Pattern.matches("^(\\+\\d{2()?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$", mNum);
+			return mobileNum;
+		}
+		public boolean password1(String password) {
+			boolean pwd1 = Pattern.matches("^([0-9a-zA-Z@#!]){8,}$", password);
+			return pwd1;
+		}
 
+		public boolean password2(String password) {
+			boolean pwd2 = Pattern.matches("^([a-z0-9@#!]*)[A-Z]+([a-z0-9@#!]*)$", password);
+			return pwd2;
+		}
+
+		public boolean password3(String password) {
+			boolean pwd3 = Pattern.matches("^([a-zA-Z@#!]*)[0-9]+([a-zA-Z@#!]*)$", password);
+			return pwd3;
+		}
+
+		public boolean password4(String password) {
+			boolean pwd4 = Pattern.matches("^([a-zA-Z0-9]*)[@#!$]{1}([a-zA-Z0-9]*)$", password);
+			return pwd4;
+		}
 }
