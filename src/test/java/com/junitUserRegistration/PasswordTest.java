e2030ab7941754a25d18bd8112fd9dfe97c223cb
Rachana <rachanarrachu28@gmail.com>
2020-10-27 21:50:41 +0530

    Added : Password Condition Checked

---
+++ src/test/java/com/junitUserRegistration/PasswordTest.java
@@ -0,0 +1,90 @@
+package com.junitUserRegistration;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PasswordTest {
+
+	static UserRegistration user;
+
+	@BeforeClass
+	public static void createUserRegistrationObj() {
+		user = new UserRegistration();
+		System.out.println("In before class: Password");
+	}
+
+	@AfterClass
+	public static void nullObj() {
+		System.out.println("In After class: Password");
+		user = null;
+	}
+
+	@Test
+	public void testPasswordWithMinimumEightCharacters_testAssertionHappy() {
+		boolean result = user.password1("Rachanar");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void testPasswordWithoutMinimumEightCharacters_testAssertionSad() {
+		boolean result = user.password1("Rachan");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void testPasswordWithMinimumEightCharacters_testAssertionSad() {
+		boolean result = user.password1("87965646");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void testPasswordWithAtleastOneUpperCase_testAssertionHappy() {
+		boolean result = user.password2("RachanaR");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void testPasswordWithoutAtleastOneUpperCase_testAssertionSad() {
+		boolean result = user.password2("rachu");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void testPasswordWithAtleastOneUpperCase_testAssertionSad() {
+		boolean result = user.password2("skwndkxS");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void testPasswordWithAtleastOneNumeric_testAssertionHappy() {
+		boolean result = user.password3("RachanaR28");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void testPasswordWithoutAtleastOneNumeric_testAssertionSad() {
+		boolean result = user.password3("Rachanar");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void testPasswordWithAtleastOneNumeric_testAssertionSad() {
+		boolean result = user.password3("9skwndkx");
+		Assert.assertFalse(result);
+	}
+
+	@Test
+	public void testPasswordWithExactlyOneSpecialCharacter_testAssertionHappy() {
+		boolean result = user.password4("Rachu@123");
+		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void testPasswordWithoutExactlyOneSpecialCharacter_testAssertionSad() {
+		boolean result = user.password4("rachana");
+		Assert.assertFalse(result);
+	}
+
+}
+
